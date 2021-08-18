e81de68ff52e002aedfa2c07ee07bbeeabfd5f7b
TheUselessGoddess <50678711+TheUselessGoddess@users.noreply.github.com>
2021-08-18 14:17:25 -0500

    Add files via upload

---
+++ Printing.py
@@ -0,0 +1,21 @@
+"""
+
+    Program 1: Printing a song or poem
+
+"""
+
+print("spOwOky OwOky pumpkin")
+print("he a owange, woundo boi")
+print("he woll and wobbwe wound de patch")
+print("wooking fow a tastee snac!\n")
+
+print("spOwOky OwOky pumpkin")
+print("Eyes so fuww of fwight!")
+print("he'w bite youw toes n then u know")
+print("he's a evil boi tonite\n")
+
+print("wotund widdle sqwashy one")
+print("you'w nevew undowhstand")
+print("u fink u'w supew spOwOky")
+print("but weawwy u'w just mad!")
+
