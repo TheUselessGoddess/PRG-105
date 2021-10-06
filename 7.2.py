feea51b683e79c2f1f581b206012915375decefa
Nick T <50678711+TheUselessGoddess@users.noreply.github.com>
2021-10-06 13:27:03 -0500

    Add files via upload

---
+++ 7.2.py
@@ -0,0 +1,19 @@
+"""
+Using parallel arrays, create a secret code creator.
+Ask the user for text to enter, convert it to the code and write the code to a text file. Include punctuation (. , !), space, upper, and lower case letters.
+"""
+
+def main():
+    normal = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ', '!', '?', '.', ',']
+    code = [' ', '.', '?', '!', ',', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B']
+    file = open('code.txt', 'w')
+    phrase = input("Please enter a phrase to be translated into a secret phrase.")
+    changed = ""
+    for letter in phrase:
+        for item in range(0, len(normal)):
+            if letter.upper() == normal[item]:
+                changed += code[item] + "\n"
+                file.write(code[item])
+
+    print(changed)
+main()
