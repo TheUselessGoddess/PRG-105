6c302c04badd239368fa3fb8576fb867ebd1ebcb
Nick T <50678711+TheUselessGoddess@users.noreply.github.com>
2021-11-22 12:58:18 -0600

    Add files via upload

--- 9.2.py
+++ 9.2.py
@@ -4,18 +4,15 @@
 
 def main():
     print('Enter the number in English which corresponds to the French one.')
-    numbers = ['un', 'deux', 'trois', 'quatre', 'cinq', 'six', 'sept', 'huit', 'neuf', 'dix']
-    numbers2 = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
+    numbers = {'un': 'one', 'deux': 'two', 'trois': 'three', 'quatre': 'four', 'cinq': 'five', 'six': 'six', 'sept': 'seven', 'huit': 'eight', 'neuf': 'nine', 'dix': 'ten'}
     score = 0
-    count = 0
-    for x in numbers:
+    for x in numbers.keys():
         answer = input('What is the equivalent of ' + x + ': ')
-        if answer.lower() == numbers2[count]:
+        if answer.lower() == numbers[x]:
             print('Correct')
             score += 1
         else:
-            print('Incorrect. The answer was ' + numbers2[count])
-        count += 1
+            print('Incorrect. The answer was ' + numbers[x])
     print('You got ' + str(score))
     if score >= 9:
         print('A')
